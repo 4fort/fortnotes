b1c79e13767918dbe89c894fa9680c0c4afcf73e
Dexter Fort <dexterkun16@gmail.com>
2023-10-15 16:47:53 +0800

    updated dialogue design

--- src/components/AddNote.tsx
+++ src/components/AddNote.tsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import * as Form from "@radix-ui/react-form";
 import * as Checkbox from "@radix-ui/react-checkbox";
 import * as Dialog from "@radix-ui/react-dialog";
-import { TbPin, TbPinFilled, TbPinnedOff, TbX } from "react-icons/tb";
+import {
+  TbDeviceFloppy,
+  TbPin,
+  TbPinFilled,
+  TbPinnedOff,
+  TbX,
+} from "react-icons/tb";
 
 const AddNote = () => {
   const [title, setTitle] = useState<string>("");
@@ -10,7 +16,6 @@ const AddNote = () => {
   const [isPinned, setIsPinned] = useState<
     "checked" | "unchecked" | "indeterminate"
   >("indeterminate");
-  const [formError, setFormError] = useState(null);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -19,34 +24,36 @@ const AddNote = () => {
 
   return (
     <>
-      <Dialog.Title className='text-xl mb-4'>AddNote</Dialog.Title>
+      <Dialog.Title className='text-neutral-500/80 mb-4'>AddNote</Dialog.Title>
       <Form.Root className='flex flex-col gap-5' onSubmit={handleSubmit}>
-        <Form.Field className='flex flex-col' name='title'>
-          <Form.Label className='text-neutral-500'>Title</Form.Label>
-          <Form.Control asChild>
-            <input
-              className='bg-neutral-800/40 p-2 rounded-md focus:outline-none'
-              type='text'
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </Form.Control>
-        </Form.Field>
+        <div className='overflow-clip rounded-md bg-neutral-800/30'>
+          <Form.Field className='flex flex-col' name='title'>
+            <Form.Control asChild>
+              <input
+                className='bg-transparent focus:bg-neutral-800/20 text-2xl font-semibold p-2 focus:outline-none border-b-2 border-b-neutral-800'
+                type='text'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Control>
+          </Form.Field>
 
-        <Form.Field className='flex flex-col' name='context'>
-          <Form.Label className='text-neutral-500'>Context</Form.Label>
-          <Form.Control asChild>
-            <textarea
-              className='resize-none bg-neutral-800/40 p-2 rounded-md focus:outline-none'
-              rows={4}
-              value={context}
-              onChange={(e) => setContext(e.target.value)}
-            />
-          </Form.Control>
-        </Form.Field>
+          <Form.Field className='flex flex-col' name='context'>
+            <Form.Control asChild>
+              <textarea
+                className='bg-transparent focus:bg-neutral-800/20 font-thin p-2 resize-none focus:outline-none'
+                rows={10}
+                value={context}
+                onChange={(e) => setContext(e.target.value)}
+              />
+            </Form.Control>
+          </Form.Field>
+        </div>
 
         <Form.Submit asChild>
-          <button>Add note</button>
+          <button className='flex justify-center items-center gap-2 p-2 bg-emerald-700 rounded-md'>
+            <TbDeviceFloppy /> Save
+          </button>
         </Form.Submit>
       </Form.Root>
       <div className='absolute -top-3 -right-3 flex shadow-sm text-xl bg-emerald-700 rounded-md overflow-hidden before:content-[""] before:absolute before:w-[1.2px] before:h-4/6 before:inset-x-1/2 before:self-center before:flex before:bg-neutral-600/40'>
